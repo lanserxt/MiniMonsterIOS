35f310489e5e71b33b41cec8962563d641b7223d
Anton Gubarenko <anton.gubarenko.dev@gmail.com>
2015-12-09 12:54:25 +0300

    Settings VC added

---
+++ MiniMonsterIOS/MiniMonsterIOS/Controllers/MMPDeviceSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMPDeviceSettingsViewController.h
+//  MiniMonsterIOS
+//
+//  Created by Anton Gubarenko on 09.12.15.
+//  Copyright © 2015 Anton Gubarenko. All rights reserved.
+//
+
+#import "MMPBaseViewController.h"
+
+@interface MMPDeviceSettingsViewController : MMPBaseViewController
+
+@end
---
+++ MiniMonsterIOS/MiniMonsterIOS/Controllers/MMPDeviceSettingsViewController.m
@@ -0,0 +1,13 @@
+//
+//  MMPDeviceSettingsViewController.m
+//  MiniMonsterIOS
+//
+//  Created by Anton Gubarenko on 09.12.15.
+//  Copyright © 2015 Anton Gubarenko. All rights reserved.
+//
+
+#import "MMPDeviceSettingsViewController.h"
+
+@implementation MMPDeviceSettingsViewController
+
+@end
